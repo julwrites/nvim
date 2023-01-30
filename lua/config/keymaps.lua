27a0666356af83116cd31d84ac82a8bf23a5ee39
julwrites <mail@tehj.io>
2023-01-30 22:17:27 +0800

    Updating timestamp keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,7 +5,7 @@ function Config()
   nvim.set_keymap("n", "<C-\\><C-v><C-e>", ":e $MYVIMRC<CR>", {}) -- Timestamp
   nvim.set_keymap("n", "<C-\\><C-v><C-s>", ":source $MYVIMRC<CR>", {}) -- Timestamp
 
-  nvim.set_keymap("i", "<C-t><C-s>", "a<C-R>=strftime('%Y-%m-%d %a %I:%M %p')<CR><Esc>", {}) -- Timestamp
+  nvim.set_keymap("i", "<C-t><C-s>", "<C-R>=strftime('%Y-%m-%d %a %I:%M %p')<CR><Esc>", {}) -- Timestamp
 
 end
 
