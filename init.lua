bbd58605bef655fb51e68858694f97b5abdcc0f7
Julian <julian.teh@screeningeagle.com>
2023-02-21 15:24:27 +0800

    Removing python spec for MacOS

--- init.lua
+++ init.lua
@@ -18,15 +18,6 @@ bootstrap("norcalli", "nvim.lua")
 print("Bootstrapping Packer.nvim")
 bootstrap("wbthomason", "packer.nvim")
 
--- Stuff that needs to be set before plugins run
-local nvim = require("nvim")
-local conda_env = "envs/labelImg"
-
-if vim.loop.os_uname().sysname == "Darwin" then
-  nvim.g.python3_host_prog = "/usr/local/Caskroom/miniconda/base/" .. conda_env .. "/bin/python3"
-end
-
-
 local plug = require("config.plugins")
 plug.update()
 
