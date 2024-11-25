009001781f49bc3446953a6f0770dd19e9420362
julian <julian.teh@screeningeagle.com>
2024-11-25 17:11:14 +0800

    Updating with conform

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -327,12 +327,14 @@ function Update()
         require("conform").setup({
           formatters_by_ft = {
             lua = { "stylua" },
-            -- Conform will run multiple formatters sequentially
             python = { "isort", "black" },
-            -- You can customize some of the format options for the filetype (:help conform.format)
             rust = { "rustfmt", lsp_format = "fallback" },
-            -- Conform will run the first available formatter
+            cpp = { "clang-format" },
             javascript = { "prettierd", "prettier", stop_after_first = true },
+          },
+          format_on_save = {
+            timeout_ms = 500,
+            lsp_format = "fallback",
           },
         })
       end
