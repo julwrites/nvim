c424894742e8e325ee7f479e689e93d4d1c47d9e
julian <julian.teh@screeningeagle.com>
2025-03-26 16:15:18 +0800

    Adding Magenta as an alternative to CodeCompanion

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -106,6 +106,7 @@ function Update()
       config = function()
         -- calling `setup` is optional for customization
         require("fzf-lua").setup({})
+        require("fzf-lua").register_ui_select()
       end
     },
     {
@@ -166,16 +167,7 @@ function Update()
       version = "*", -- recommended, use latest release instead of latest commit
       lazy = true,
       ft = "markdown",
-      -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-      -- event = {
-      --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-      --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-      --   -- refer to `:h file-pattern` for more examples
-      --   "BufReadPre path/to/my-vault/*.md",
-      --   "BufNewFile path/to/my-vault/*.md",
-      -- },
       dependencies = {
-        -- Required.
         "nvim-lua/plenary.nvim",
       },
       opts = {
@@ -572,7 +564,7 @@ function Update()
           "lua_ls",
           "bashls",
           "pyright",
-          "tsserver",
+          "ts_ls",
           "volar",
           "astro",
           "sourcekit",
@@ -654,6 +646,13 @@ function Update()
           }
         })
       end
+    },
+
+    {
+      "dlants/magenta.nvim",
+      lazy = false, -- you could also bind to <leader>mt
+      build = "npm install --frozen-lockfile",
+      opts = {},
     },
 
     {
